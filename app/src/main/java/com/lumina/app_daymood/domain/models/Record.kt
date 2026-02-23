593fa119fcc8b67f998e5688322a3812748b7aab
mili-emzz <m37009146@gmail.com>
2026-02-22 21:30:55 -0600

    dtos para record, emotion, habit. Cambio en los nombres de los modelos para mejor identifiacion

--- app/src/main/java/com/lumina/app_daymood/domain/models/Record.kt
+++ app/src/main/java/com/lumina/app_daymood/domain/models/Record.kt
@@ -2,13 +2,13 @@ package com.lumina.app_daymood.domain.models
 
 import com.google.firebase.Timestamp
 
-class Record(
+class RecordModel(
     val id: String,
     val userId: String,
     val date: String,
     val note: String? = null,
-    val emotion: Emotion,
-    val habits: List<Habit> = emptyList(),
+    val emotion: EmotionModel,
+    val habits: List<HabitModel> = emptyList(),
 ) {
 
 }
