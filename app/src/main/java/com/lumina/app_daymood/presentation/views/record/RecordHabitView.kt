4e71a9fa5c11c606670a4cc5c7a74e4ca41e2bf1
mili-emzz <m37009146@gmail.com>
2026-02-23 22:05:42 -0600

    recirdemotion view actualizado

--- app/src/main/java/com/lumina/app_daymood/presentation/views/record/RecordHabitView.kt
+++ app/src/main/java/com/lumina/app_daymood/presentation/views/record/RecordHabitView.kt
@@ -1,10 +1,12 @@
 package com.lumina.app_daymood.presentation.views.record
 
 import com.lumina.app_daymood.presentation.viewmodels.RecordViewModel
+import java.time.LocalDate
 
 class RecordHabitView(
     recordViewModel: RecordViewModel,
     onBackClick: () -> Unit,
-    onSaveSuccess: () -> Unit
+    onSaveSuccess: () -> Unit,
+    date: LocalDate
 ) {
 }
